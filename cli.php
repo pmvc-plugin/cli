5eb4a39a62c9c2ada6626d24e31475999baa2afe
Hill <hill@kimo.com>
2021-05-14 12:55:50 +0000

    Add auto get app with pwd

--- cli.php
+++ cli.php
@@ -4,14 +4,12 @@ namespace PMVC\PlugIn\cli;
 
 use PMVC\NamespaceAdapter;
 
-${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\cli';
+${_INIT_CONFIG}[_CLASS] = __NAMESPACE__ . '\cli';
 
-\PMVC\l(__DIR__.'/src/Color2.php');
-\PMVC\initPlugIn(['controller'=>null]);
+\PMVC\l(__DIR__ . '/src/Color2.php');
+\PMVC\initPlugIn(['controller' => null]);
 
-class cli
-    extends \PMVC\PlugIn
-    implements \PMVC\RouterInterface
+class cli extends \PMVC\PlugIn implements \PMVC\RouterInterface
 {
     private $_color;
 
@@ -20,17 +18,28 @@ class cli
         $controller = \PMVC\plug('controller');
         $opts = $this->getopt();
         $request = $controller->getRequest();
-        foreach($opts as $k=>$v){
+        foreach ($opts as $k => $v) {
             if (!is_numeric($k)) {
-                $request[$k] = $v; 
-            } elseif ($k>1) {
+                $request[$k] = $v;
+            } elseif ($k > 1) {
                 $request[] = $v;
             }
         }
         if (empty($opts[1])) {
-            return;
-        }else{
-            $app = explode(':',$opts[1]);
+            $appsFolder = \PMVC\lastSlash($controller->getAppsFolder());
+            $pwd = getcwd();
+            $start = strpos($pwd, $appsFolder);
+            if (0 === $start) {
+                $sApp = explode('/', substr($pwd, strlen($appsFolder)));
+                if (!empty($sApp[0])) {
+                    $app = [$sApp[0]];
+                }
+            }
+            if (empty($app)) {
+                return;
+            }
+        } else {
+            $app = explode(':', $opts[1]);
         }
         if (isset($app[0])) {
             $controller->setApp($app[0]);
@@ -42,34 +51,36 @@ class cli
 
     public function init()
     {
-        \PMVC\callPlugin(
-            'dispatcher',
-            'attach',
-            array(
-                $this,
-                \PMVC\Event\MAP_REQUEST
-            )
-        );
+        \PMVC\callPlugin('dispatcher', 'attach', [
+            $this,
+            \PMVC\Event\MAP_REQUEST,
+        ]);
         $this->_color = new Console_Color2();
         $this->setDefaultAlias(new NamespaceAdapter('cli'));
     }
 
-    public function color($color,$text)
+    public function color($color, $text)
     {
         if (is_array($text) || is_object($text)) {
-            $text = var_export($text,true);
+            $text = var_export($text, true);
         }
         $text = $this->_color->escape($text);
-        return $this->_color->convert($color.$text.'%n');
+        return $this->_color->convert($color . $text . '%n');
     }
 
-    public function dump($text,$color="%m")
+    public function dump($text, $color = '%m')
     {
-        echo $this->color($color,$text)."\n";
+        echo $this->color($color, $text) . "\n";
     }
 
     // abstract function
-    public function buildCommand($path, $params) { }
-    public function processHeader(array $headers) { }
-    public function go($path, $isClientLocation = false) { }
+    public function buildCommand($path, $params)
+    {
+    }
+    public function processHeader(array $headers)
+    {
+    }
+    public function go($path, $isClientLocation = false)
+    {
+    }
 }
